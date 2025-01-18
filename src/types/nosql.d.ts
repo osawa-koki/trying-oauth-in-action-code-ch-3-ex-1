2e8df39d67d9a50279eace117e06e7b2a1e25012
osawa-koki <koki@osawa.cloud>
2025-01-18 16:41:23 +0900

    lint fix

--- src/types/nosql.d.ts
+++ src/types/nosql.d.ts
@@ -1,6 +1,12 @@
 declare module 'nosql' {
+  interface TokenData {
+    access_token: string;
+    client_id: string;
+    scope: string | null;
+  }
+
   interface Database {
-    insert: (data: any) => void;
+    insert: (data: TokenData) => void;
     clear: () => void;
   }
 
